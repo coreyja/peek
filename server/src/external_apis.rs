7c8586faedca9d3f54fa0ef67346b80520b9838e
Corey Alexander <coreyja@gmail.com>
2022-10-07 19:59:34 -0400

    Cleanup

--- server/src/external_apis.rs
+++ server/src/external_apis.rs
@@ -2,5 +2,5 @@
 ///
 /// We are on the free Trial Tier, which limits us to 1,000 requests per month.
 /// We can also only make 3 requests per second.
-/// https://www.microsoft.com/en-us/bing/apis/pricing
+/// <https://www.microsoft.com/en-us/bing/apis/pricing>
 pub mod bing_news;
